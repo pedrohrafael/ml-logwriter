83de4e24c60df3825cbb11615f49260d2a8791be
Pedro Henrique Rafael <pedro.rodrigues.rafael@gmail.com>
2023-07-19 23:52:40 -0300

    updates

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
-from setuptools import setup
-  
+
+from setuptools import setup, find_packages
+setup(name='pathology',
+      version='0.1',
+      url='https://github.com/the-gigi/pathology',
+      license='MIT',
+      author='Gigi Sayfan',
+      author_email='the.gigi@gmail.com',
+      description='Add static script_dir() method to Path',
+      packages=find_packages(exclude=['tests']),
+      long_description=open('README.md').read(),
+      zip_safe=False)
+    
 setup(
     name='ml_logwriter',
     version='0.1',
@@ -7,7 +18,7 @@ setup(
     description='A ML logger package',
     author='Pedro H Rafael',
     author_email='pedro.rodrigues.rafael@gmail.com',
-    packages=['ml_logwriter'],
+    packages=find_packages(),
     install_requires=[
         'numpy',
         'pandas',
