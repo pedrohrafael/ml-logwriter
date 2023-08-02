96944986ce86bd055dea7996383d9f007124f84b
Pedro Henrique Rafael <pedro.rodrigues.rafael@gmail.com>
2023-08-02 10:48:18 -0300

    update v0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ml_logwriter',
-    version='0.2.2',
+    version='0.2.3',
     url='https://github.com/pedrohrafael/ml-logwriter',
     license='MIT',
     description='A ML logger package',
