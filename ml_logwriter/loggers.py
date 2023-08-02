713c0330c45b1155602a5f2f67d89cf9b2ed933c
Pedro Henrique Rafael <59976208+pedrohrafael@users.noreply.github.com>
2023-08-01 23:42:29 -0300

    Update loggers.py

--- ml_logwriter/loggers.py
+++ ml_logwriter/loggers.py
@@ -33,26 +33,26 @@ class _CustomFormatter(logging.Formatter):
 class LogWriter:    
     """
     Logging package for Python.
-        
+
     Methods
     -------
-    logger(path) :
-        Return a register and create a file at the specified path.    
+    logger(path):
+        Return a logger and create a file at the specified path.
     """
     
     def logger(self, path):
         """
-        Return a register and create a file at the specified path.
-        
+        Return a logger and create a file at the specified path.
+
         Parameters
-        -------
-        path : str, required
-            directory path on will create the log file.
+        ----------
+        path : str
+            Directory path where the log file will be created.
         """
         self.log_path = path
         StreamHandler = logging.StreamHandler()
         StreamHandler.setFormatter(_CustomFormatter())
-        
+
         logging.basicConfig(level=logging.DEBUG,
                             format=LOG_FORMAT,
                             datefmt=LOG_DATEFMT,
@@ -63,33 +63,42 @@ class LogWriter:
 class LogArtifacts:
     """
     Logging artifacts package for Python.
-        
+
     Methods
     -------
-    create(name) :
-    load(name) : 
-    log_parameters(parameters) : 
-    log_metrics(metrics) : 
-    log_model(model) :
-    log_dataset(dataframe, name) :
+    create(name=None):
+    load(name):
+    log_parameters(parameters):
+    log_metrics(metrics):
+    log_model(model):
+    log_dataset(dataframe, name):
     """
     
     def __init__(self, path):
         """
-        Logging artifacts package for Python.
+        Create a LogArtifacts object.
+
         Parameters
-        -------
-        path : str, required
-            root directory path on will create the other directories by methods.
+        ----------
+        path : str
+            Root directory path where other directories will be created by methods.
         """
-        self.root_artifacts_path=path
+        self.root_artifacts_path = path
      
     def create(self, name=None):
+        """
+        Create a new artifacts directory with the specified name or a random name.
+
+        Parameters
+        ----------
+        name : str, optional
+            Name of the artifacts directory. If not provided, a random name will be generated.
+        """
         if name:
             if name not in os.listdir(self.root_artifacts_path):
                 self.name = name
             else:
-                sys.exit(f"Diretório já existente:{os.path.join(self.root_artifacts_path, name)}")
+                sys.exit(f"Directory already exists: {os.path.join(self.root_artifacts_path, name)}")
         else:
             self.name = hashlib.md5(str(random.getrandbits(50)).encode('utf-8')).hexdigest()
         self.artifacts_path = os.path.join(self.root_artifacts_path, self.name)
@@ -99,9 +108,25 @@ class LogArtifacts:
             sys.exit(e)
         
     def load(self, name):
+        """
+        Load an existing artifacts directory.
+
+        Parameters
+        ----------
+        name : str
+            Name of the existing artifacts directory to load.
+        """
         self.artifacts_path = os.path.join(self.root_artifacts_path, name)
         
     def log_parameters(self, params):
+        """
+        Log model parameters.
+
+        Parameters
+        ----------
+        params : dict
+            Dictionary containing model parameters to be logged.
+        """
         directory = 'parameters'
         path = os.path.join(self.artifacts_path, directory)
         try:
@@ -115,6 +140,14 @@ class LogArtifacts:
                 f.write(str(value))
     
     def log_metrics(self, metrics):
+        """
+        Log model evaluation metrics.
+
+        Parameters
+        ----------
+        metrics : dict
+            Dictionary containing model evaluation metrics to be logged.
+        """
         directory = 'metrics'
         path = os.path.join(self.artifacts_path, directory)
         try:
@@ -128,6 +161,14 @@ class LogArtifacts:
                 f.write(str(value))
    
     def log_model(self, model):
+        """
+        Save the trained model to the artifacts directory.
+
+        Parameters
+        ----------
+        model : object
+            Trained model object to be saved.
+        """
         directory = 'artifacts'
         path = os.path.join(self.artifacts_path, directory)
         try:
@@ -140,17 +181,24 @@ class LogArtifacts:
         self._log_meta()
     
     def log_dataset(self, dataframe, name):
-            """
-            Save dataframe as CSV.
-            """
-            directory = 'datasets'
-            path = os.path.join(self.artifacts_path, directory)
-            try:
-                if not directory in os.listdir(self.artifacts_path):
-                    os.mkdir(path)
-                dataframe.to_csv(os.path.join(path, name + '.csv'), index=False)
-            except Exception as e:
-                sys.exit(e)            
+        """
+        Save the DataFrame as a CSV file in the artifacts directory.
+
+        Parameters
+        ----------
+        dataframe : pandas.DataFrame
+            DataFrame to be saved as a CSV file.
+        name : str
+            Name of the CSV file to be saved.
+        """
+        directory = 'datasets'
+        path = os.path.join(self.artifacts_path, directory)
+        try:
+            if not directory in os.listdir(self.artifacts_path):
+                os.mkdir(path)
+            dataframe.to_csv(os.path.join(path, name + '.csv'), index=False)
+        except Exception as e:
+            sys.exit(e)            
                        
     def _log_meta(self):
         create_date = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
